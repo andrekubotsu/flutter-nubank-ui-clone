8de681b21b4eafa3660de0eb74f6a47c3ec35ddb
Andre Kubotsu <contato201@gmail.com>
2021-09-05 09:47:00 -0300

    added: constants and project organization

---
+++ lib/shared/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const purple = Color(0xFF820ad1);
+  static const lightGrey = Color(0xFFF6F5FA);
+  static const black = Color(0xFF191919);
+}
